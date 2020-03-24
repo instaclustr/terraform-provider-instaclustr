575c9ff8f4b2d110781b9317b7a30c7025575a43
EngineersBox <35655145+EngineersBox@users.noreply.github.com>
2020-03-24 14:53:25 +1100

    Un-commented examples for VPC peering and firewall rules

--- examples/main.tf
+++ examples/main.tf
@@ -42,19 +42,19 @@ resource "instaclustr_cluster" "example2" {
   }
 }
 
-# resource "instaclustr_firewall_rule" "test3" {
-#   cluster_id = "${instaclustr_cluster.example2.id}"
-#   rule_cidr = "10.1.0.0/16"
-#   rules = [
-#     {
-#       type = "CASSANDRA"
-#     }
-#   ]
-# }
+resource "instaclustr_firewall_rule" "example_firewall_rule" {
+  cluster_id = "${instaclustr_cluster.example2.id}"
+  rule_cidr = "10.1.0.0/16"
+  rules = [
+    {
+      type = "CASSANDRA"
+    }
+  ]
+}
 
-# resource "instaclustr_vpc_peering" "example_vpc_peering" {
-#   cluster_id = "${instaclustr_cluster.example.cluster_id}"
-#   peer_vpc_id = "vpc-123456"
-#   peer_account_id = "1234567890"
-#   peer_subnet = "10.0.0.0/20"
-# }
+resource "instaclustr_vpc_peering" "example_vpc_peering" {
+  cluster_id = "${instaclustr_cluster.example.cluster_id}"
+  peer_vpc_id = "vpc-123456"
+  peer_account_id = "1234567890"
+  peer_subnet = "10.0.0.0/20"
+}
