b07eb19acd1c96d65df7d2d2a6084fca077671d9
Arjun Raj <arjun.raj@instaclustr.com>
2020-03-27 17:20:18 +1100

    chore: added example TF configuration usage of custom_virtual_network_id

--- examples/main.tf
+++ examples/main.tf
@@ -43,6 +43,29 @@ resource "instaclustr_cluster" "example2" {
   }
 }
 
+resource "instaclustr_cluster" "custom_vpc_example" {
+  cluster_name = "testcluster"
+  node_size = "t3.small"
+  data_centre = "US_WEST_2"
+  sla_tier = "NON_PRODUCTION"
+  cluster_network = "10.100.0.0/16"
+  private_network_cluster = false
+  cluster_provider = {
+    name = "AWS_VPC",
+    account_name = "My RIYOA Account"
+    custom_virtual_network_id = "vpc-0a1b2c3d4e5f6g7h8"
+  }
+  rack_allocation = {
+    number_of_racks = 2
+    nodes_per_rack = 1
+  }
+
+  bundle {
+    bundle = "APACHE_CASSANDRA"
+    version = "3.11.4"
+  }
+}
+
 resource "instaclustr_firewall_rule" "example_firewall_rule" {
   cluster_id = "${instaclustr_cluster.example2.id}"
   rule_cidr = "10.1.0.0/16"
