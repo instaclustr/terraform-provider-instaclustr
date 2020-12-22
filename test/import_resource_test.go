01a93becce5bad74f9332fa1daa4ff22d8c6b446
stephenma <stephen.ma@anu.edu.au>
2020-12-22 16:38:09 +1100

    TST: implemented an acceptance test for importable cluster resource

---
+++ test/import_resource_test.go
@@ -0,0 +1,43 @@
+package test
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/instaclustr/terraform-provider-instaclustr/instaclustr"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestAccCluster_importBasic(t *testing.T) {
+	testAccProvider := instaclustr.Provider()
+	testAccProviders := map[string]terraform.ResourceProvider{
+		"instaclustr": testAccProvider,
+	}
+	validConfig, _ := ioutil.ReadFile("data/valid_single_bundle.tf")
+	username := os.Getenv("IC_USERNAME")
+	apiKey := os.Getenv("IC_API_KEY")
+	hostname := getOptionalEnv("IC_API_URL", instaclustr.DefaultApiHostname)
+	oriConfig := fmt.Sprintf(string(validConfig), username, apiKey, hostname)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { AccTestEnvVarsCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckResourceDeleted("valid", hostname, username, apiKey),
+		Steps: []resource.TestStep{
+			{
+				Config: oriConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckResourceValid("valid"),
+					testCheckResourceCreated("valid", hostname, username, apiKey),
+				),
+			},
+			{
+				Config:            oriConfig,
+				ResourceName:      "instaclustr_cluster.valid",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
