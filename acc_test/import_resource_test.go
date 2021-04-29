0cd64344ce13bd22c8f3e1f7ce9c3aef4d7571c3
stephenma <stephen.ma@anu.edu.au>
2021-04-29 23:34:33 +1000

    TestAccMultiDcCluster_importBasic.

--- acc_test/import_resource_test.go
+++ acc_test/import_resource_test.go
@@ -16,6 +16,38 @@ func TestAccCluster_importBasic(t *testing.T) {
 	testAccProviders := map[string]terraform.ResourceProvider{
 		"instaclustr": testAccProvider,
 	}
+	validConfig, _ := ioutil.ReadFile("data/valid.tf")
+	username := os.Getenv("IC_USERNAME")
+	apiKey := os.Getenv("IC_API_KEY")
+	hostname := getOptionalEnv("IC_API_URL", instaclustr.DefaultApiHostname)
+	oriConfig := fmt.Sprintf(string(validConfig), username, apiKey, hostname)
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckResourceDeleted("valid", hostname, username, apiKey),
+		Steps: []resource.TestStep{
+			{
+				Config: oriConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckResourceValid("valid"),
+					testCheckResourceCreated("valid", hostname, username, apiKey),
+				),
+			},
+			{
+				Config:            oriConfig,
+				ResourceName:      "instaclustr_cluster.valid",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccMultiDcCluster_importBasic(t *testing.T) {
+
+	testAccProvider := instaclustr.Provider()
+	testAccProviders := map[string]terraform.ResourceProvider{
+		"instaclustr": testAccProvider,
+	}
 	validConfig, _ := ioutil.ReadFile("data/valid_multi_dc_provisioning.tf")
 	username := os.Getenv("IC_USERNAME")
 	apiKey := os.Getenv("IC_API_KEY")
