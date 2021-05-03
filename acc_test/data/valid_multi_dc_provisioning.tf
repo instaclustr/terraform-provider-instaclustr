bae7782f6f642cd21147e552ade0e96627bb8edb
stephenma <stephen.ma@anu.edu.au>
2021-05-03 11:19:17 +1000

    terraform fmt.

--- acc_test/data/valid_multi_dc_provisioning.tf
+++ acc_test/data/valid_multi_dc_provisioning.tf
@@ -1,47 +1,47 @@
 provider "instaclustr" {
-  username = "%s"
-  api_key = "%s"
+  username     = "%s"
+  api_key      = "%s"
   api_hostname = "%s"
 }
 
 resource "instaclustr_cluster" "valid" {
   cluster_name = "testcluster"
-  node_size = "t3.small-v2"
-  sla_tier = "NON_PRODUCTION"
+  node_size    = "t3.small-v2"
+  sla_tier     = "NON_PRODUCTION"
   cluster_provider = {
     name = "AWS_VPC"
   }
   rack_allocation = {
     number_of_racks = 2
-    nodes_per_rack = 1
+    nodes_per_rack  = 1
   }
 
   data_centres {
-    name = "DC1"
+    name               = "DC1"
     data_centre_region = "US_WEST_1"
-    network = "10.0.0.0/18"
+    network            = "10.0.0.0/18"
   }
 
   data_centres {
-    name = "DC2"
+    name               = "DC2"
     data_centre_region = "US_WEST_1"
-    network = "10.1.0.0/18"
+    network            = "10.1.0.0/18"
   }
 
   data_centres {
-    name = "DC3"
+    name               = "DC3"
     data_centre_region = "US_WEST_2"
-    network = "192.168.0.0/18"
+    network            = "192.168.0.0/18"
   }
 
   bundle {
-    bundle = "APACHE_CASSANDRA"
+    bundle  = "APACHE_CASSANDRA"
     version = "apache-cassandra-3.11.8.ic2"
     options = {
-      auth_n_authz = true
-      continuous_backup_enabled = true
-      client_encryption = false
-      lucene_enabled = false
+      auth_n_authz                      = true
+      continuous_backup_enabled         = true
+      client_encryption                 = false
+      lucene_enabled                    = false
       use_private_broadcast_rpc_address = false
     }
   }
