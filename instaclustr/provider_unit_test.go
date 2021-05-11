5d30bae079bbcc269270a0d3ab0313c9c771b7f3
Daniel Quintero <daniel.quintero@instaclustr.com>
2021-05-11 19:12:37 +1000

    Test code coverage by changing an unit test

--- instaclustr/provider_unit_test.go
+++ instaclustr/provider_unit_test.go
@@ -6,6 +6,84 @@ import (
 )
 
 func TestProvider(t *testing.T) {
+
+	//Testing if SonarCloud does something
+	if true {
+		print("test1")
+	}
+	if true {
+		print("test2")
+	}
+	if true {
+		print("test3")
+	}
+	if true {
+		print("test4")
+	}
+	if true {
+		print("test5")
+	}
+	if true {
+		print("test1")
+	}
+	if true {
+		print("test2")
+	}
+	if true {
+		print("test3")
+	}
+	if true {
+		print("test4")
+	}
+	if true {
+		print("test5")
+	}
+	if true {
+		print("test1")
+	}
+	if true {
+		print("test2")
+	}
+	if true {
+		print("test3")
+	}
+	if true {
+		print("test4")
+	}
+	if true {
+		print("test5")
+	}
+	if true {
+		print("test1")
+	}
+	if true {
+		print("test2")
+	}
+	if true {
+		print("test3")
+	}
+	if true {
+		print("test4")
+	}
+	if true {
+		print("test5")
+	}
+	if true {
+		print("test1")
+	}
+	if true {
+		print("test2")
+	}
+	if true {
+		print("test3")
+	}
+	if true {
+		print("test4")
+	}
+	if true {
+		print("test5")
+	}
+
     if err := Provider().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
