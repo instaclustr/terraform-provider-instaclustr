dfff4ea87604d10eab5b564602bed69481f1be2f
Daniel Quintero <daniel.quintero@instaclustr.com>
2021-05-12 11:24:38 +1000

    Remove unit test to test code coverage

--- instaclustr/resource_cluster_unit_test.go
+++ instaclustr/resource_cluster_unit_test.go
@@ -2,77 +2,9 @@ package instaclustr
 
 import (
 	"fmt"
-	"reflect"
 	"testing"
 )
 
-func TestCreateBundleUserUpdateRequest(t *testing.T) {
-	testUsername := "hail"
-	testPassword := "reallySecure123"
-	testBundleRequest := createBundleUserUpdateRequest(testUsername, testPassword)
-
-	expectedOutput := []byte(fmt.Sprintf("{\"username\":\"%s\",\"password\":\"%s\"}", testUsername, testPassword))
-
-	if !reflect.DeepEqual(testBundleRequest, expectedOutput){
-		t.Fatalf("Incorrect request returned.\nExpected:%s\nActual:%s", expectedOutput, testBundleRequest)
-	}
-}
-
-func TestGetBundleConfig(t *testing.T) {
-	var testBundles []Bundle
-	var testBundleConfig BundleConfig
-
-	testBundles = append(testBundles, Bundle{Bundle: "KAFKA"})
-	testBundleConfig = getBundleConfig(testBundles)
-	expectedOutput := BundleConfig{
-		IsKafkaCluster: true,
-		HasRestProxy: false,
-		HasSchemaRegistry: false}
-
-	if testBundleConfig != expectedOutput {
-		t.Fatalf("Incorrect Bundle Config returned.\nExpected: %+v\nActual: %+v", expectedOutput, testBundleConfig)
-	}
-
-	testBundles = append(testBundles, Bundle{Bundle: "KAFKA_REST_PROXY"})
-	testBundleConfig = getBundleConfig(testBundles)
-	expectedOutput = BundleConfig{
-		IsKafkaCluster: true,
-		HasRestProxy: true,
-		HasSchemaRegistry: false}
-
-	if testBundleConfig != expectedOutput {
-		t.Fatalf("Incorrect Bundle Config returned.\nExpected: %+v\nActual: %+v", expectedOutput, testBundleConfig)
-	}
-
-	testBundles = append(testBundles, Bundle{Bundle: "KAFKA_SCHEMA_REGISTRY"})
-	testBundleConfig = getBundleConfig(testBundles)
-	expectedOutput = BundleConfig{
-		IsKafkaCluster: true,
-		HasRestProxy: true,
-		HasSchemaRegistry: true}
-
-	if testBundleConfig != expectedOutput {
-		t.Fatalf("Incorrect Bundle Config returned.\nExpected: %+v\nActual: %+v", expectedOutput, testBundleConfig)
-	}
-}
-
-func TestAppendIfMissing(t *testing.T) {
-	var testSlice []string
-	testSlice = append(testSlice, "1", "2")
-	testString := "test"
-
-	expectedSlice := append(testSlice, testString)
-	appendedSlice := appendIfMissing(testSlice, testString)
-
-	if !reflect.DeepEqual(expectedSlice, appendedSlice) {
-		t.Fatalf("Value appened incorrectly to the slice.\nExpected: %s\nActual: %s", expectedSlice, appendedSlice)
-	}
-
-	appendedSlice = appendIfMissing(testSlice, testString)
-	if !reflect.DeepEqual(expectedSlice, appendedSlice) {
-		t.Fatalf("Value appened incorrectly to the slice.\nExpected: %s\nActual: %s", expectedSlice, appendedSlice)
-	}
-}
 
 func TestFormatCreateErrMsg(t *testing.T) {
 	testError := fmt.Errorf("test error")
