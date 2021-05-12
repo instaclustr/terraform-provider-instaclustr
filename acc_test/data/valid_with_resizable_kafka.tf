bb7b8d83f8ee5d5d9fd7f779afea72dcfd72ac7f
Harvey Yu <yang.yu@instaclustr.com>
2021-05-13 02:24:53 +1000

    Add kafka reszie acceptance test

---
+++ acc_test/data/valid_with_resizable_kafka.tf
@@ -0,0 +1,44 @@
+provider "instaclustr" {
+    username = "%s"
+    api_key = "%s"
+    api_hostname = "%s"
+}
+resource "instaclustr_cluster" "resizable_cluster" {
+    cluster_name = "test_cluster"
+    node_size = "r5.xlarge-800-gp2"
+    data_centre = "US_WEST_2"
+    sla_tier = "NON_PRODUCTION"
+    cluster_network = "192.168.0.0/18"
+    private_network_cluster = false
+    pci_compliant_cluster = false
+    wait_for_state = "RUNNING"
+    cluster_provider = {
+        name = "AWS_VPC"
+    }
+    rack_allocation = {
+        number_of_racks = 3
+        nodes_per_rack = 1
+    }
+
+    bundle {
+        bundle = "KAFKA"
+        version = "apache-kafka:2.5.1"
+        options = {
+            client_encryption = false
+            auto_create_topics = true
+            delete_topics = true
+            number_partitions = 10
+            zookeeper_node_count = 3
+        }
+    }
+
+    bundle {
+        bundle = "KAFKA_SCHEMA_REGISTRY"
+        version = "kafka-schema-registry:5.0.0"
+    }
+    bundle {
+        bundle = "KAFKA_REST_PROXY"
+        version = "kafka-rest-proxy:5.0.0"
+    }
+
+}
