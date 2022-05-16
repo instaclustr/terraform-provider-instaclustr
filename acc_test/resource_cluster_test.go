e79a6066287c60012764336e8c0dd6ae35ab66d9
yiweiwu <yiwei.wu@instaclustr.com>
2022-05-16 21:09:40 +1000

    fix TestAccOpenSearchClusterResize

--- acc_test/resource_cluster_test.go
+++ acc_test/resource_cluster_test.go
@@ -320,8 +320,8 @@ func TestAccOpenSearchClusterResize(t *testing.T) {
 	hostname := getOptionalEnv("IC_API_URL", instaclustr.DefaultApiHostname)
 	resourceName := "resizable_cluster"
 	oriConfig := fmt.Sprintf(string(validConfig), username, apiKey, hostname)
-	validResizeConfig := strings.Replace(oriConfig, `opensearch_dashboards_node_size = "t3.small-v2"`, `opensearch_dashboards_node_size = "SRH-DEV-t4g.small-30",`, 1)
-	invalidResizeConfig := strings.Replace(oriConfig, `opensearch_dashboards_node_size = "t3.small-v2",`, `opensearch_dashboards_node_size = "t3.small",`, 1)
+	validResizeConfig := strings.Replace(oriConfig, `opensearch_dashboards_node_size = "SRH-DEV-t4g.small-5"`, `opensearch_dashboards_node_size = "SRH-DEV-t4g.small-30"`, 1)
+	invalidResizeConfig := strings.Replace(oriConfig, `opensearch_dashboards_node_size = "SRH-DEV-t4g.small-5"`, `opensearch_dashboards_node_size = "SRH-DEV-t4g.small"`, 1)
 
 	resource.ParallelTest(t, resource.TestCase{
 		Providers:    testAccProviders,
@@ -342,7 +342,7 @@ func TestAccOpenSearchClusterResize(t *testing.T) {
 					time.Sleep(1 * time.Minute)
 				},
 				Config:      invalidResizeConfig,
-				ExpectError: regexp.MustCompile("t3.small"),
+				ExpectError: regexp.MustCompile("SRH-DEV-t4g.small"),
 			},
 			{
 				Config: validResizeConfig,
